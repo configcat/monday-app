b79b0d2e2d941d79cb534c127aa104bd5a273909
novalisdenahi <novalisdenahi@gmail.com>
2025-09-17 11:06:13 +0200

    Remove dialog resize. Fix error scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, DestroyRef, ElementRef, inject, OnInit, viewChild } from "@angular/core";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { MatDialog } from "@angular/material/dialog";
+import { Component, ElementRef, inject, OnInit, viewChild } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { Theme, ThemeService } from "ng-configcat-publicapi-ui";
 
@@ -13,20 +11,11 @@ import { Theme, ThemeService } from "ng-configcat-publicapi-ui";
 export class AppComponent implements OnInit {
 
   private readonly themeService = inject(ThemeService);
-  private readonly dialog = inject(MatDialog);
-  private readonly destroyRef = inject(DestroyRef);
   readonly resizeReference = viewChild<ElementRef<HTMLElement>>("resizeReference");
 
   title = "ConfigCat Feature Flags";
 
   ngOnInit(): void {
-    this.dialog.afterOpened.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(result => {
-      this.resize(result.id);
-    });
-    this.dialog.afterAllClosed.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
-      this.resize();
-    });
-
     const darkModeOn =
     window.matchMedia
     && window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -44,19 +33,5 @@ export class AppComponent implements OnInit {
       });
   }
 
-  resize(dialogId?: string): void {
-    console.log("fix resize " + dialogId);
-    setTimeout(() => {
-      // const contentHeight = this.resizeReference()?.nativeElement?.offsetHeight;
-
-      // let height = contentHeight && contentHeight < 500 ? contentHeight : 500;
-      // if (dialogId) {
-      //   const dialogHeight = document.getElementById(dialogId)?.offsetHeight ?? 0;
-      //   // the extra 130 px is hard coded. because of the dialog content dinamically changes the height.
-      //   height = height < dialogHeight ? dialogHeight + 130 : height;
-      // }
-      // this.jiraService.resize(height);
-    }, 500);
-  }
 }
 
